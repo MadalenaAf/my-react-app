904d53c1cb54e7811699054f63a94d6c12971bfe
MadalenaAf <59823235+MadalenaAf@users.noreply.github.com>
2020-04-12 15:44:09 +0100

    Added the API icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function CurrentWeather(props) {
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
       temperature: response.data.main.temp,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
     setReady(true);
   }
